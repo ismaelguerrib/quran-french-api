fd50aced73d4186d9196b7309b934ae9ed8b8662
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2026-03-09 12:47:11 +0100

    feat: create TranslationSourceEntity with properties and relationships for translation sources

--- src/translation-sources/entities/translation-source.entity.ts
+++ src/translation-sources/entities/translation-source.entity.ts
@@ -1,1 +1,30 @@
-export class TranslationSource {}
+import {
+  Column,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { AyahTranslationEntity } from '../../ayah-translations/entities/ayah-translation.entity';
+
+@Entity('translation_sources')
+@Index(['code'], { unique: true })
+export class TranslationSourceEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  code!: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  label?: string;
+
+  @Column({ type: 'varchar', length: 10, nullable: true })
+  language?: string;
+
+  @OneToMany(
+    () => AyahTranslationEntity,
+    (translation) => translation.translationSource,
+  )
+  translations!: AyahTranslationEntity[];
+}
